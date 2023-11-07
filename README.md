4d765a706010bd9fbc2f2805366c46bd345af64c
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2023-11-06 20:11:06 -0500

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # Unit2.PartyPlanner
+
+## Party Planner
+
+The Party Planner is a web application that allows users to manage and keep track of party events. It provides functionality to view a list of upcoming parties with detailed information, add new parties to the schedule, and delete parties that are no longer needed.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Criteria](#criteria)
+- [Contributing](#contributing)
+
+## Installation
+
+To get started with the Party Planner app locally, follow these steps:
+
+```bash
+# Clone the repository
+git clone https://github.com/yourusername/Unit2.PartyPlanner.git
+
+# Navigate to the project directory
+cd Unit2.PartyPlanner
+
+# Open the index.html file in your preferred browser
+```
+
+## Usage
+
+1. Open the `index.html` in your browser to view the app's main interface.
+2. The list of parties will be displayed on the homepage.
+3. Use the form provided to enter details of a new party and click the 'Add Party' button to schedule it.
+4. Click the 'Delete' button next to any party you wish to remove from the list.
+
+## Features
+
+- **View Parties**: A detailed list of all scheduled parties is available for users to browse.
+- **Add New Party**: Users can add a new party to the list using a simple form.
+- **Delete Party**: Users can remove a party from the list with a single click.
+- **Responsive Layout**: The app is designed to be accessible on a variety of devices and screen sizes.
+
+## Criteria
+
+The Party Planner app fulfills the following criteria:
+
+1. **Fetch**: Utilizes `fetch` to GET party data from the API.
+2. **Fetch**: Uses `fetch` to POST a new party to the API.
+3. **Fetch**: Implements `fetch` to DELETE a party from the API.
+4. **Functionality**: Displays a list of parties including names, dates, times, locations, and descriptions.
+5. **Functionality**: Incorporates a delete button for each party to remove it from the list.
+6. **Functionality**: Features a form that lets users add information about a new party.
+7. **Code Architecture**: Dynamically renders the DOM based on the application's state.
+8. **Code Architecture**: Maintains state in sync with the API.
+
+## Contributing
+
+If you're interested in contributing to the Party Planner project, your input is welcome! For substantial changes, please open an issue first to discuss what you would like to improve. Pull requests are equally appreciated.
---
+++ index.js
@@ -0,0 +1,116 @@
+// JavaScript File: index.js
+
+const EVENTS_URI =
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-PT-WEB-PT-A/events";
+
+const state = {
+  events: [],
+};
+
+// Fetch and display all parties
+const getParties = async () => {
+  try {
+    const response = await fetch(EVENTS_URI);
+    const json = await response.json();
+    if (json.error) throw new Error(json.error);
+    state.events = json.data;
+    renderEvents();
+  } catch (error) {
+    console.error('GET Error:', error);
+  }
+};
+
+// Add a new party
+const createParty = async (partyData) => {
+  try {
+    const response = await fetch(EVENTS_URI, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(partyData),
+    });
+    const json = await response.json();
+    if (json.error) throw new Error(json.error);
+    getParties(); // Refresh the list of parties
+  } catch (error) {
+    console.error('POST Error:', error);
+  }
+};
+
+// Delete a party
+const deleteParty = async (id) => {
+  try {
+    const response = await fetch(`${EVENTS_URI}/${id}`, { method: "DELETE" });
+    if (!response.ok) throw new Error('Delete was not successful');
+    getParties(); // Refresh the list of parties
+  } catch (error) {
+    console.error('DELETE Error:', error);
+  }
+};
+
+// Update a party
+const updateParty = async (id, updatedData) => {
+  try {
+    const response = await fetch(`${EVENTS_URI}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedData),
+    });
+    const json = await response.json();
+    if (json.error) throw new Error(json.error);
+    getParties(); // Refresh the list of parties
+  } catch (error) {
+    console.error('PUT Error:', error);
+  }
+};
+
+// Event listener for form submission
+document.querySelector("#create-party form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const partyData = {
+    name: formData.get('title'),
+    description: formData.get('description'),
+    date: formData.get('date'),
+    location: formData.get('location'),
+  };
+  const partyId = formData.get('id');
+  if (partyId) {
+    updateParty(partyId, partyData);
+  } else {
+    createParty(partyData);
+  }
+  event.target.reset(); // Clear the form
+});
+
+// Render all parties to the DOM
+const renderEvents = () => {
+  const partiesList = document.querySelector("#display-parties");
+  partiesList.innerHTML = ''; // Clear the current list
+  state.events.forEach(party => {
+    const partyElement = document.createElement("li");
+    partyElement.innerHTML = `
+      <h2>${party.name}</h2>
+      <p>${new Date(party.date).toLocaleString()}</p>
+      <p>${party.location}</p>
+      <p>${party.description}</p>
+      <button onclick="deleteParty(${party.id})">Delete</button>
+      <button onclick="populateEditForm(${party.id})">Edit</button>
+    `;
+    partiesList.appendChild(partyElement);
+  });
+};
+
+// Populate the edit form with party details
+const populateEditForm = (id) => {
+  const party = state.events.find(event => event.id === id);
+  if (!party) return;
+  const form = document.querySelector("#create-party form");
+  form.title.value = party.name;
+  form.date.value = party.date;
+  form.location.value = party.location;
+  form.description.value = party.description;
+  form.id.value = party.id; // Assuming there is an input field in the form for the id
+};
+
+// Initialize the application by fetching the parties
+getParties();
